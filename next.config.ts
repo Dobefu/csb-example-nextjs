eee21d59ee0c4325e3729edd5f549f033b368787
Connor van Spronssen <info@dobefu.com>
2024-12-23 16:03:06 +0100

    feat: Add some security headers to the Next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,23 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
