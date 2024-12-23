31207c18ca5b0571bac46c8f4124794239b743ff
Connor van Spronssen <info@dobefu.com>
2024-12-23 16:51:35 +0100

    feat: Add a script to fetch all locales from the API

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,35 @@
 import type { NextConfig } from 'next'
+import { fetchLocales } from './src/fetch-locales'
 
-const nextConfig: NextConfig = {
-  output: 'standalone',
-  reactStrictMode: true,
-  poweredByHeader: false,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-        ],
-      },
-    ]
-  },
+module.exports = async () => {
+  const { defaultLocale, locales } = await fetchLocales()
+
+  const nextConfig: NextConfig = {
+    output: 'standalone',
+    reactStrictMode: true,
+    poweredByHeader: false,
+    i18n: {
+      defaultLocale,
+      locales,
+    },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+          ],
+        },
+      ]
+    },
+  }
+
+  return nextConfig
 }
-
-export default nextConfig
---
+++ src/fetch-locales.ts
@@ -0,0 +1,59 @@
+type ApiResponse = {
+  data: {
+    locales: {
+      code: string
+      fallback_locale: string
+      name: string
+    }[]
+  }
+  error: string | null
+}
+
+export async function fetchLocales(): Promise<{
+  defaultLocale: string
+  locales: string[]
+}> {
+  const endpoint = process.env.CS_API_ENDPOINT
+  const deliveryToken = process.env.CS_DELIVERY_TOKEN
+
+  if (!endpoint || !deliveryToken) {
+    return {
+      defaultLocale: 'en',
+      locales: ['en'],
+    }
+  }
+
+  const response = await fetch(`${endpoint}/locales`, {
+    headers: {
+      authorization: deliveryToken,
+    },
+  })
+
+  if (!response.ok) {
+    return {
+      defaultLocale: 'en',
+      locales: ['en'],
+    }
+  }
+
+  const { data, error }: ApiResponse = await response.json()
+
+  if (error) {
+    return {
+      defaultLocale: 'en',
+      locales: ['en'],
+    }
+  }
+
+  let defaultLocale = 'en'
+
+  const locales = data.locales.map((locale) => {
+    if (locale.fallback_locale) {
+      defaultLocale = locale.fallback_locale
+    }
+
+    return locale.code
+  })
+
+  return { defaultLocale, locales }
+}
