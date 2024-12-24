15699ad7bb922f89bc68ee793de95a285cec85d8
Connor van Spronssen <info@dobefu.com>
2024-12-24 11:43:36 +0100

    feat: Add the eslint-plugin-react package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import { FlatCompat } from '@eslint/eslintrc'
+import pluginReact from 'eslint-plugin-react'
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
@@ -8,6 +9,12 @@ const eslintConfig = [
   ...compat.config({
     extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
   }),
+  {
+    ...pluginReact.configs.flat['jsx-runtime'],
+    rules: {
+      'react/jsx-sort-props': ['error'],
+    },
+  },
 ]
 
 export default eslintConfig
