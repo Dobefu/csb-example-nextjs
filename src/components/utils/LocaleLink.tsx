8e5f15ae93e7c6760f82c71b07736c06f540be7a
Connor van Spronssen <info@dobefu.com>
2024-12-24 13:32:06 +0100

    feat: Add a localeOverride prop to the LocaleLink component

--- src/components/utils/LocaleLink.tsx
+++ src/components/utils/LocaleLink.tsx
@@ -4,15 +4,19 @@ import { LocaleContext } from '@/app/[locale]/providers'
 import NextLink, { LinkProps } from 'next/link'
 import React, { useContext } from 'react'
 
-const LocaleLink: React.FC<LinkProps & React.HTMLProps<HTMLAnchorElement>> = (
-  props,
-) => {
+const LocaleLink: React.FC<
+  LinkProps &
+    React.HTMLProps<HTMLAnchorElement> &
+    Readonly<{ localeOverride?: string }>
+> = (props) => {
   const locale = useContext(LocaleContext)
 
   const newProps = {
     ...props,
-    href: `/${locale}${props.href}`,
+    href: `/${props.localeOverride || locale}${props.href}`,
   }
+
+  delete newProps.localeOverride
 
   return <NextLink {...newProps}>{newProps.children}</NextLink>
 }
