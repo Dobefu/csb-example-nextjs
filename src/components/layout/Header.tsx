56caf652dae3168b641b4698196572f6d93f0f79
Connor van Spronssen <info@dobefu.com>
2024-12-24 12:27:49 +0100

    feat: Slightly style the Header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,14 @@
 import getConfig from 'next/config'
+import Link from 'next/link'
 
 export default function Header() {
   const { publicRuntimeConfig } = getConfig()
 
-  return <header className="p-4">{publicRuntimeConfig.appName}</header>
+  return (
+    <header className="flex justify-between gap-4 p-4 text-2xl font-medium">
+      <nav>
+        <Link href="/">{publicRuntimeConfig.appName}</Link>
+      </nav>
+    </header>
+  )
 }
