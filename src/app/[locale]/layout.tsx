c37e20ad0e1dd8520a5b24703fb1d13234c3c855
Connor van Spronssen <info@dobefu.com>
2024-12-24 10:42:50 +0100

    feat: Add the footer to the layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/layout/Footer'
 import Header from '@/components/layout/Header'
 import type { Metadata } from 'next'
 import { Geist } from 'next/font/google'
@@ -29,6 +30,8 @@ export default async function RootLayout({
         <Header />
 
         {children}
+
+        <Footer />
       </body>
     </html>
   )
