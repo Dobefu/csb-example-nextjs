2fb27318bdca7d4cde936aa43f8c569c767eb85b
Connor van Spronssen <info@dobefu.com>
2024-12-24 16:22:45 +0100

    feat: Center the footer text

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,5 +3,9 @@ import getConfig from 'next/config'
 export default function Footer() {
   const { publicRuntimeConfig } = getConfig()
 
-  return <footer className="p-4">{publicRuntimeConfig.appName}</footer>
+  return (
+    <footer className="p-4 text-center text-gray-600">
+      {publicRuntimeConfig.appName}
+    </footer>
+  )
 }
