d3d3378c8af0e22abdff6256e7fa205316a635a9
Connor van Spronssen <info@dobefu.com>
2024-12-25 12:38:59 +0100

    feat: Add an extra check if the API endpoint exists to the test

--- src/utils/query/get-page-by-url.ts
+++ src/utils/query/get-page-by-url.ts
@@ -2,6 +2,11 @@ import { createQueryClient } from './create-query-client'
 
 export async function getPageByUrl<T>(pageUrl: string, locale: string) {
   const endpoint = process.env.CS_API_ENDPOINT ?? ''
+
+  if (!endpoint) {
+    return { data: null, error: 'CS_API_ENDPOINT is not valid' }
+  }
+
   const url = `${endpoint}/get-entry-by-url?url=${pageUrl}&locale=${locale}`
 
   const queryClient = createQueryClient()
