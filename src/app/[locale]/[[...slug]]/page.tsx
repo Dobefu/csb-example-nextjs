98fa569d778e218630988d8e6c6f28eb80a125d9
Connor van Spronssen <info@dobefu.com>
2024-12-25 12:14:00 +0100

    feat: Add data fetching to the page

--- src/app/[locale]/[[...slug]]/page.tsx
+++ src/app/[locale]/[[...slug]]/page.tsx
@@ -1,7 +1,18 @@
+import { logError } from '@/utils/logger'
+import { getPageByUrl } from '@/utils/query/get-page-by-url'
+import { notFound } from 'next/navigation'
+
 export default async function Home({
   params,
 }: Readonly<{
   params: Promise<{ locale: string; slug?: string[] }>
 }>) {
-  return <>{`/${((await params).slug ?? []).join('/')}`}</>
+  const { data, error } = await getPageByUrl('/', 'en')
+
+  if (error) {
+    logError(`Cannot get the page: ${error}`)
+    return notFound()
+  }
+
+  return <code className="whitespace-pre">{JSON.stringify(data, null, 2)}</code>
 }
