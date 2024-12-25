04f2d094c7adeed9b8018cf21902f602d0d25289
Connor van Spronssen <info@dobefu.com>
2024-12-25 16:25:47 +0100

    test: Ignore coverage for the early return in the logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,9 @@
 export function logError(message: string) {
+  /* v8 ignore start */
   if (process.env.NODE_ENV === 'test') {
     return
   }
+  /* v8 ignore end */
 
   console.error(`‼ ${message}`)
 }
