1d44a70830010eeb69ca3839a24773cc32f1469f
Connor van Spronssen <info@dobefu.com>
2024-12-25 14:58:24 +0100

    feat: Use the new Entry type in the RoutableEntryResponse

--- src/types/routable-entry-response.ts
+++ src/types/routable-entry-response.ts
@@ -1,3 +1,5 @@
+import { Entry } from './entry'
+
 export interface RoutableEntryResponse {
   data: {
     alt_locales: {
@@ -7,31 +9,7 @@ export interface RoutableEntryResponse {
       slug: string
       url: string
     }[]
-    entry: {
-      ACL: unknown
-      _in_progress: boolean
-      _version: number
-      created_at: string
-      created_by: string
-      locale: string
-      parent: {
-        _content_type_uid: string
-        uid: string
-      }[]
-      publish_details: {
-        environment: string
-        locale: string
-        time: string
-        user: string
-      }
-      tags: string[]
-      title: string
-      uid: string
-      updated_at: string
-      updated_by: string
-      url: string
-      [key: string]: unknown
-    }
+    entry: Entry
   } | null
   error: string | null
 }
