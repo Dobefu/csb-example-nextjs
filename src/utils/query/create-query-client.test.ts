148ff7de988cfe3a0a013679e4b7093d8899af72
Connor van Spronssen <info@dobefu.com>
2024-12-25 10:33:36 +0100

    test: Add a second test to the createQueryClient function

--- src/utils/query/create-query-client.test.ts
+++ src/utils/query/create-query-client.test.ts
@@ -10,4 +10,15 @@ describe('createQueryClient', () => {
       client.getDefaultOptions().queries?.staleTime,
     ).toBeGreaterThanOrEqual(1000)
   })
+
+  it('creates a QueryClient without any cache', () => {
+    process.env.DISABLE_QUERY_CACHE = '1'
+
+    const client = createQueryClient()
+
+    expect(client).toBeDefined()
+    expect(client.getDefaultOptions().queries?.staleTime).toBe(1)
+
+    delete process.env.DISABLE_QUERY_CACHE
+  })
 })
