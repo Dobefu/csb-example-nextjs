c9b23c70f18fd04131262be1b7edc34dad063657
Connor van Spronssen <info@dobefu.com>
2024-12-25 16:22:25 +0100

    test: Add some tests to the formatMetadata function

---
+++ src/utils/format-metadata.test.ts
@@ -0,0 +1,113 @@
+import { AltLocale } from '@/types/alt-locale'
+import { Entry } from '@/types/entry'
+import { beforeEach, describe, expect, it } from 'vitest'
+import formatMetadata from './format-metadata'
+
+let mockEntry: Entry
+let mockAltLocales: AltLocale[] = []
+
+describe('format-metadata', () => {
+  beforeEach(() => {
+    mockEntry = {
+      ACL: {},
+      _in_progress: false,
+      _version: 1,
+      created_at: '',
+      created_by: '',
+      locale: 'en',
+      parent: [],
+      publish_details: {
+        environment: 'main',
+        locale: 'en',
+        time: '',
+        user: '',
+      },
+      tags: [],
+      title: 'Test Title',
+      uid: '',
+      updated_at: '',
+      updated_by: '',
+      url: '/',
+      seo: undefined,
+    } satisfies Entry
+
+    mockAltLocales = [
+      {
+        uid: '',
+        content_type: '',
+        locale: 'nl',
+        slug: '/',
+        url: '/',
+      } satisfies AltLocale,
+    ]
+  })
+
+  it('formats the metadata correctly', () => {
+    const metadata = formatMetadata(mockEntry, mockAltLocales)
+
+    expect(metadata).toMatchObject({
+      alternates: {
+        canonical: '/',
+        languages: {
+          nl: '/nl/',
+        },
+      },
+      description: undefined,
+      openGraph: {
+        description: undefined,
+        title: 'Test Title',
+      },
+      title: 'Test Title',
+    })
+  })
+
+  it('formats the metadata correctly with SEO data', () => {
+    mockEntry['seo'] = {
+      description: 'Test Description',
+      og_description: 'Test OG Description',
+      og_title: 'Test OG Title',
+      title: 'Test SEO Title',
+    }
+
+    const metadata = formatMetadata(mockEntry, mockAltLocales)
+
+    expect(metadata).toMatchObject({
+      alternates: {
+        canonical: '/',
+        languages: {
+          nl: '/nl/',
+        },
+      },
+      description: 'Test Description',
+      openGraph: {
+        description: 'Test OG Description',
+        title: 'Test OG Title',
+      },
+      title: 'Test SEO Title',
+    })
+  })
+
+  it('formats the metadata correctly with partial SEO data', () => {
+    mockEntry['seo'] = {
+      description: 'Test Description',
+      og_title: 'Test OG Title',
+    }
+
+    const metadata = formatMetadata(mockEntry, mockAltLocales)
+
+    expect(metadata).toMatchObject({
+      alternates: {
+        canonical: '/',
+        languages: {
+          nl: '/nl/',
+        },
+      },
+      description: 'Test Description',
+      openGraph: {
+        description: 'Test Description',
+        title: 'Test OG Title',
+      },
+      title: 'Test Title',
+    })
+  })
+})
