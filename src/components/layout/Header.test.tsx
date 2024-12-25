33c644196d9e1ea13b79bbf936175fac523dd77a
Connor van Spronssen <info@dobefu.com>
2024-12-25 19:08:43 +0100

    test: Fix broken tests

--- src/components/layout/Header.test.tsx
+++ src/components/layout/Header.test.tsx
@@ -1,6 +1,24 @@
+import { AltLocale } from '@/types/alt-locale'
 import { cleanup, render, screen } from '@testing-library/react'
 import { afterEach, describe, expect, it, vi } from 'vitest'
 import Header from './Header'
+
+const mockAltLocales: AltLocale[] = [
+  {
+    uid: '',
+    content_type: '',
+    locale: 'en',
+    slug: '/',
+    url: '/',
+  },
+  {
+    uid: '',
+    content_type: '',
+    locale: 'nl',
+    slug: '/',
+    url: '/',
+  },
+]
 
 describe('Header', () => {
   afterEach(() => {
@@ -9,7 +27,7 @@ describe('Header', () => {
   })
 
   it('Renders', () => {
-    render(<Header />)
+    render(<Header altLocales={mockAltLocales} />)
 
     expect(screen.getByRole('banner')).toBeDefined()
   })
--- src/components/layout/LocaleSwitcher.test.tsx
+++ src/components/layout/LocaleSwitcher.test.tsx
@@ -1,7 +1,25 @@
+import { AltLocale } from '@/types/alt-locale'
 import { cleanup, fireEvent, render, screen } from '@testing-library/react'
 import { loadEnvFile } from 'node:process'
 import { afterEach, describe, expect, it } from 'vitest'
 import LocaleSwitcher from './LocaleSwitcher'
+
+const mockAltLocales: AltLocale[] = [
+  {
+    uid: '',
+    content_type: '',
+    locale: 'en',
+    slug: '/',
+    url: '/',
+  },
+  {
+    uid: '',
+    content_type: '',
+    locale: 'nl',
+    slug: '/',
+    url: '/',
+  },
+]
 
 describe('LocaleSwitcher', () => {
   afterEach(() => {
@@ -10,7 +28,7 @@ describe('LocaleSwitcher', () => {
   })
 
   it('Renders normally', async () => {
-    render(<LocaleSwitcher />)
+    render(<LocaleSwitcher altLocales={mockAltLocales} />)
 
     expect(screen.getByRole<HTMLSelectElement>('combobox').value).toBe('en')
     expect(screen.getAllByRole('option').length).toBe(2)
@@ -19,7 +37,7 @@ describe('LocaleSwitcher', () => {
   it('Redirects the root path', async () => {
     process.env.MOCK_PATHNAME = '/'
 
-    render(<LocaleSwitcher />)
+    render(<LocaleSwitcher altLocales={mockAltLocales} />)
 
     fireEvent.change(screen.getByRole<HTMLSelectElement>('combobox'))
     expect(screen.getAllByRole('option').length).toBe(2)
@@ -28,7 +46,7 @@ describe('LocaleSwitcher', () => {
   it('Redirects with a query parameter', async () => {
     process.env.MOCK_PATHNAME = '/?test=1'
 
-    render(<LocaleSwitcher />)
+    render(<LocaleSwitcher altLocales={mockAltLocales} />)
 
     fireEvent.change(screen.getByRole<HTMLSelectElement>('combobox'))
     expect(screen.getAllByRole('option').length).toBe(2)
