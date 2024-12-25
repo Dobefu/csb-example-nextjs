7a085d515a33eb1615a29e376bae7e20ec9bf07b
Connor van Spronssen <info@dobefu.com>
2024-12-25 11:45:48 +0100

    fix: Remove a console.log

--- src/utils/query/get-page-by-url.ts
+++ src/utils/query/get-page-by-url.ts
@@ -2,7 +2,7 @@ import { createQueryClient } from './create-query-client'
 
 export async function getPageByUrl<T>(pageUrl: string, locale: string) {
   const url = `${process.env.CS_API_ENDPOINT ?? ''}/get-entry-by-url?url=${pageUrl}&locale=${locale}`
-  console.log(url)
+
   const queryClient = createQueryClient()
   const query = {
     queryKey: ['getPageByUrl', url],
