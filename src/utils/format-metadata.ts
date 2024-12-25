8a6d23141e6e9cace48990cd56d6dc3df5beef76
Connor van Spronssen <info@dobefu.com>
2024-12-25 15:56:14 +0100

    feat: Add a function to format page metadata

---
+++ src/utils/format-metadata.ts
@@ -0,0 +1,27 @@
+import { AltLocale } from '@/types/alt-locale'
+import { Entry } from '@/types/entry'
+import { Metadata } from 'next'
+
+export default function formatMetadata(
+  entry: Entry,
+  altLocales: AltLocale[],
+): Metadata {
+  const altLocalesMeta: Record<string, string> = {}
+
+  for (const altLocale of altLocales) {
+    altLocalesMeta[altLocale.locale] = `/${altLocale.locale}${altLocale.url}`
+  }
+
+  return {
+    title: entry.seo?.title ?? entry.title,
+    description: entry.seo?.description,
+    alternates: {
+      canonical: '/',
+      languages: altLocalesMeta,
+    },
+    openGraph: {
+      title: entry.seo?.og_title ?? entry.title,
+      description: entry.seo?.description,
+    },
+  }
+}
