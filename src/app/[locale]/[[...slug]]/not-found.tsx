6ffb8ce40e9c54dbf722f4aaaed83e5d9b379f24
Connor van Spronssen <info@dobefu.com>
2024-12-26 12:03:08 +0100

    test: Add some tests to the NotFound route

--- src/app/[locale]/[[...slug]]/not-found.tsx
+++ src/app/[locale]/[[...slug]]/not-found.tsx
@@ -4,7 +4,9 @@ export default function NotFound() {
   return (
     <div className="flex flex-col gap-2">
       <h1 className="text-xl font-medium">Not Found</h1>
+
       <p>Could not find requested resource</p>
+
       <LocaleLink className="text-blue-600" href="/">
         Return Home
       </LocaleLink>
