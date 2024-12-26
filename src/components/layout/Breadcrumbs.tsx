d5c583aceafb8dc7e0d3a4161fe0d5a3c2a34d7b
Connor van Spronssen <info@dobefu.com>
2024-12-26 07:49:54 +0100

    feat: Add slashes to the breadcrumbs

--- src/components/layout/Breadcrumbs.tsx
+++ src/components/layout/Breadcrumbs.tsx
@@ -1,4 +1,5 @@
 import { EntryPreview } from '@/types/entry-preview'
+import { cn } from '@/utils/cn'
 import LocaleLink from '../utils/LocaleLink'
 
 type Props = {
@@ -8,9 +9,15 @@ type Props = {
 export default function Breadcrumbs({ breadcrumbs }: Readonly<Props>) {
   return (
     <div className="px-8">
-      <ol className="after:[/] flex gap-4">
-        {breadcrumbs.map((breadcrumb) => (
-          <li key={breadcrumb.uid}>
+      <ol className="flex gap-2">
+        {breadcrumbs.map((breadcrumb, index) => (
+          <li
+            className={cn(
+              'after:ps-2 after:font-medium after:text-gray-400',
+              index !== breadcrumbs.length - 1 && "after:content-['/']",
+            )}
+            key={breadcrumb.uid}
+          >
             <LocaleLink href={breadcrumb.url}>{breadcrumb.title}</LocaleLink>
           </li>
         ))}
