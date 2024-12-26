d298d5dddf14bdc650eb25560fcfcdc9dd759bfb
Connor van Spronssen <info@dobefu.com>
2024-12-26 06:54:06 +0100

    feat: Add a type for an entry preview

---
+++ src/types/entry-preview.ts
@@ -0,0 +1,11 @@
+export interface EntryPreview {
+  id: string
+  uid: string
+  content_type: string
+  locale: string
+  slug: string
+  url: string
+  parent: string
+  exclude_sitemap: boolean
+  published: boolean
+}
--- src/types/routable-entry-response.ts
+++ src/types/routable-entry-response.ts
@@ -1,9 +1,11 @@
 import { AltLocale } from './alt-locale'
 import type { Entry } from './entry'
+import { EntryPreview } from './entry-preview'
 
 export interface RoutableEntryResponse {
   data: {
     alt_locales: AltLocale[]
+    breadcrumbs: EntryPreview[]
     entry: Entry
   } | null
   error: string | null
