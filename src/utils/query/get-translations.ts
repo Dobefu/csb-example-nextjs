af89d26dd714570a82d30d7ca609238ca813efad
Connor van Spronssen <info@dobefu.com>
2024-12-29 14:24:12 +0100

    feat: Add a function to fetch all translations

---
+++ src/utils/query/get-translations.ts
@@ -0,0 +1,37 @@
+import { RoutableEntryResponse } from '@/types/routable-entry-response'
+import { createQueryClient } from './create-query-client'
+
+interface Response {
+  data: unknown | null
+  error: string | null
+}
+
+export async function getTranslations(locale: string): Promise<Response> {
+  const endpoint = process.env.CS_API_ENDPOINT ?? ''
+  const deliveryToken = process.env.CS_DELIVERY_TOKEN ?? ''
+
+  if (!endpoint) {
+    return {
+      data: null,
+      error: 'CS_API_ENDPOINT is not valid',
+    } satisfies RoutableEntryResponse
+  }
+
+  const url = `${endpoint}/translations?locale=${locale}`
+
+  const queryClient = createQueryClient()
+  const query = {
+    queryKey: ['getTranslations', url, deliveryToken],
+    queryFn: async () => {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          Authorization: deliveryToken,
+        },
+      })
+      return await response.json()
+    },
+  }
+
+  return await queryClient.fetchQuery<Response>(query)
+}
