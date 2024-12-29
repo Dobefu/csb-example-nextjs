b2a18bae8d1b13e556936ef60a1849c2c6486863
Connor van Spronssen <info@dobefu.com>
2024-12-29 12:51:37 +0100

    feat: Add a CSP header to the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,9 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(redirectUrl)
   }
 
-  return NextResponse.next()
+  const response = getCspResponse(request)
+
+  return response
 }
 
 function handleLocaleDetection(request: NextRequest): NextURL | undefined {
@@ -26,6 +28,48 @@ function handleLocaleDetection(request: NextRequest): NextURL | undefined {
 
   request.nextUrl.pathname = `/${locale}${pathname}`
   return request.nextUrl
+}
+
+function getCspResponse(request: Request): NextResponse {
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+
+  const csp = {
+    'default-src': ["'self'"],
+    'script-src': ["'self'", `'nonce-${nonce}'`, "'strict-dynamic'"],
+    'style-src': ["'self'", `'nonce-${nonce}'`],
+    'img-src': ["'self'", 'blob:', 'data:'],
+    'font-src': ["'self'"],
+    'object-src': ["'none'"],
+    'base-uri': ["'self'"],
+    'form-action': ["'self'"],
+    'frame-ancestors': ["'none'"],
+    'upgrade-insecure-requests': [],
+  }
+
+  const cspString = parseCsp(csp)
+
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('x-nonce', nonce)
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  })
+
+  response.headers.set('Content-Security-Policy', cspString)
+
+  return response
+}
+
+function parseCsp(csp: Record<string, string[]>): string {
+  const output = []
+
+  for (const [key, value] of Object.entries(csp)) {
+    output.push(`${key} ${value.join(' ')}`.trimEnd())
+  }
+
+  return output.join('; ') + ';'
 }
 
 export const config = {
