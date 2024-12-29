44b3b48e142be12b0d5f457b2a05f1de2c58b8de
Connor van Spronssen <info@dobefu.com>
2024-12-29 14:28:10 +0100

    feat: Add the translations to the locale provider

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,11 @@
 import getLocales from '@/utils/get-locales'
+import { logError } from '@/utils/logger'
+import { getTranslations } from '@/utils/query/get-translations'
 import type { Metadata } from 'next'
 import getConfig from 'next/config'
 import { TemplateString } from 'next/dist/lib/metadata/types/metadata-types'
 import { Geist } from 'next/font/google'
+import { notFound } from 'next/navigation'
 import './globals.css'
 import Providers from './providers'
 
@@ -36,12 +39,21 @@ export default async function RootLayout({
   const locales = getLocales()
   const locale = locales.find((loc) => (loc.code = pageLocale))!
 
+  const { data: translations, error } = await getTranslations(locale.code)
+
+  if (error) {
+    logError(`Could not get the translations: ${error}`)
+    notFound()
+  }
+
   return (
     <html className="h-full" lang={locale.code}>
       <body
         className={`${geistSans.variable} flex min-h-full flex-col font-sans antialiased`}
       >
-        <Providers locale={locale}>{children}</Providers>
+        <Providers locale={locale} translations={translations}>
+          {children}
+        </Providers>
       </body>
     </html>
   )
--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -5,17 +5,20 @@ import React, { createContext } from 'react'
 
 export const LocaleContext = createContext<{
   locale?: Locale
-}>({})
+  translations: Record<string, string> | null
+}>({ translations: null })
 
 export default function Providers({
   children,
   locale,
+  translations,
 }: Readonly<{
   children: React.ReactNode
   locale: Locale
+  translations: Record<string, string> | null
 }>) {
   return (
-    <LocaleContext.Provider value={{ locale }}>
+    <LocaleContext.Provider value={{ locale, translations }}>
       {children}
     </LocaleContext.Provider>
   )
