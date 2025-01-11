4d4ff7c2283fccf102610de2fd1ea4a83b0ca583
Connor van Spronssen <info@dobefu.com>
2025-01-11 11:15:53 +0100

    feat: Add an apiKey prop to the LivePreviewInit component

--- src/components/utils/LivePreviewInit.client.tsx
+++ src/components/utils/LivePreviewInit.client.tsx
@@ -3,9 +3,21 @@
 import ContentstackLivePreview from '@contentstack/live-preview-utils'
 import { useEffect } from 'react'
 
-export default function LivePreviewInit() {
+interface Props {
+  apiKey: string
+}
+
+export default function LivePreviewInit({ apiKey }: Props) {
   useEffect(() => {
-    ContentstackLivePreview.init({})
+    ContentstackLivePreview.init({
+      ssr: true,
+      enable: true,
+      mode: 'builder',
+      stackDetails: {
+        apiKey: apiKey,
+        environment: 'production',
+      },
+    })
   }, [])
 
   return <>{}</>
